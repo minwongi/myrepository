29f48de286094cf069c46b41904c375d407f125c
minwongi <alsdnjsrl223@naver.com>
2021-11-01 09:30:39 +0900

    PHP_study1

---
+++ php_study/02/quot2.php
@@ -0,0 +1,16 @@
+<?php
+	$name = "홍지수";
+	echo "$name 님 반갑습니다!";
+	echo "<br>";
+
+	$name = '홍지수';
+	echo '$name 님 반갑습니다!';	
+	echo "<br>";
+
+	$name = "홍지수";
+	echo '$name 님 반갑습니다!';	
+	echo "<br>";
+	
+	$name = '홍지수';
+	echo "$name 님 반갑습니다!";	
+?>
